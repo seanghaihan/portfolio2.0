162f9e29f32b4b1f9b6fb86955322f95117e5ece
btongheng <102462410+btongheng@users.noreply.github.com>
2024-05-24 16:37:13 +0700

    create social

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,9 @@
+import Social from "@/components/ui/Social"
+import { Button } from "@/components/ui/button"
+import {FiDownload} from "react-icons/fi"
+
 export default function Home() {
+
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
@@ -10,6 +15,20 @@ export default function Home() {
                   Hello I'm <br /><span className="text-accent">Tongheng</span>
                 </h1>
                 <p className="max-w-[500px] mb-9 text-white/80">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora fugiat maxime repellat iste dignissimos maiores sapiente? Enim, ullam?</p>
+                <div className="flex flex-col lg:flex-row items-center justify-between gap-8">
+                  <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2">
+                    <span>Download CV</span>
+                    <FiDownload className="text-xl"/>
+                  </Button>
+                  <div className="mb-8 lg:mb-0">
+                    <Social
+                    containerStyles="flex gap-6"
+                    iconStyles="w-9 h-9 border border-accent rounded-lg flex items-center justify-center text-accent text-base hover:bg-accent hover:text-primary hover:transiton-all hover:duration-300" />
+                  </div>
+                </div>
               </div>
               {/* Photo */}
               <div>Photo</div>
---
+++ components/ui/Social.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import { FiFacebook, FiInstagram, FiGithub } from "react-icons/fi"
+
+const socials = [
+    {
+        icon: <FiFacebook />, path: ""
+    },
+    {
+        icon: <FiInstagram />, path: ""
+    },
+    {
+        icon: <FiGithub />, path: ""
+    },
+    {
+        icon: <FiFacebook />, path: ""
+    },
+]
+
+export default function Social({ containerStyles, iconStyles }) {
+    return <div className={containerStyles}>
+        {socials.map(item =>
+            <Link key={item.icon} href={item.path} className={iconStyles}>
+                {item.icon}
+            </Link>
+
+        )}
+    </div>
+}
