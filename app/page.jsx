70a965516561142dfa1d8ddfe4aa804657fcf400
btongheng <102462410+btongheng@users.noreply.github.com>
2024-05-24 15:45:05 +0700

    add content on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
     <section className="h-full">
-      <div className="container mx-auto">HomePage</div>
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col lg:flex-row items-center justify-between lg:pt-8 lg:pb-24">
+              {/* text */}
+              <div className="text-center lg:text-left">
+                <span className="text-xl">Graphic Designer</span>
+                <h1 className="h1">
+                  Hello I'm <br /><span className="text-accent">Tongheng</span>
+                </h1>
+                <p className="max-w-[500px] mb-9 text-white/80">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora fugiat maxime repellat iste dignissimos maiores sapiente? Enim, ullam?</p>
+              </div>
+              {/* Photo */}
+              <div>Photo</div>
+        </div>
+      </div>
     </section>
   )
 }
