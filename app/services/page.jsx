a0679bef348e52490f5b2a162d18fd1f3c92b22b
btongheng <102462410+btongheng@users.noreply.github.com>
2024-05-25 14:38:09 +0700

    update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from 'next/link'
-import { BsArrowDownRight } from 'react-icons/bs'
+import { BsArrowDown, BsArrowDownRight } from 'react-icons/bs'
 import { motion } from 'framer-motion'
 
 export default function Services() {
@@ -15,7 +15,7 @@ export default function Services() {
     },
     {
       num: "02",
-      title: "Front-End Development",
+      title: "Front-End",
       desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, labore et dolore magna aliqua.",
       href: "",
     },
@@ -37,33 +37,34 @@ export default function Services() {
     <section className="min-h-[80vh] flex flex-col items-center justify-center py-12 lg:py-0">
       <div className="container mx-auto">
         <motion.div
-        initial={{opacity: 0}}
-        animate={{opacity: 1,
-          transition: {
-            delay: 0.9,
-            duration: 0.3,
-            ease: "easeIn"
-          }
-        }}
-        className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              delay: 0.9,
+              duration: 0.3,
+              ease: "easeIn"
+            }
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px] items-start"
         >
-          {services.map(service => 
-            <div key={service.num} className="flex flex-1 flex-col justify-center gap-6 group">
+          {services.map(service =>
+            <Link key={service.num} href={service.href} className="flex flex-1 flex-col justify-center gap-6 group">
               {/* top */}
               <div className="w-full flex justify-between items-center">
                 <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-300">{service.num}</div>
-                <Link href={service.href}>
-                  <BsArrowDownRight 
-                  className="group-hover:text-accent transition-all duration-300"/>
-                </Link>
+
+                <BsArrowDown
+                  className="group-hover:text-accent transition-all duration-300" />
+
               </div>
               {/* title */}
               <h2 className="group-hover:text-accent transition-all duration-300">{service.title}</h2>
               {/* desc */}
-              <p>{service.desc}</p>
+              <p className='text-white/80'>{service.desc}</p>
               {/* border */}
               <div className="border-b border-white/20 w-full"></div>
-            </div>
+            </Link>
           )}
         </motion.div>
       </div>
